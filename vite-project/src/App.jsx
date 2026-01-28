759980ff77174ac5bb4b53e995ecae639bc8c36a
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-01-28 23:15:07 +0200

    added the div of the new page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-import MainContent from './ReactFacts_Projects/MainContent'
-import Header from './ReactFacts_Projects/Header'
+// import MainContent from './ReactFacts_Projects/MainContent'
+// import Header from './ReactFacts_Projects/Header'
+import Header from './TravellingJournal/Header'
+import Entry from './TravellingJournal/Entry'
 function App() {
   return (
     <>
-      <div className='container'>
+      {/* <div className='container'>
         <Header/>
         <MainContent />
+      </div> */}
+
+
+
+      <div className='fulah'>
+        <Header />
+        <Entry />
       </div>
     </>
   )
