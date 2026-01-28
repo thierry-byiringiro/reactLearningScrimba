bb5b70a0841989ea920f12184cb55b38206f3e45
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-01-28 23:13:30 +0200

    done Entry page of the project

---
+++ vite-project/src/TravellingJournal/Entry.jsx
@@ -0,0 +1,19 @@
+import map from '../assets/map.png'
+const Entry = () => {
+    return (
+        <div className='light'>
+            <div><p>the image</p></div>
+            <div className='save'>
+                <div className='top'>
+                    <div>country</div>
+                    <div>maps</div>
+                </div>
+                <div>name</div>
+                <div>Date</div>
+                <div>paragraph</div>
+            </div>
+        </div>
+    );
+}
+
+export default Entry;
