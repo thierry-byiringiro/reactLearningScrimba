d0c845d27e6d5f86aabb318c14dc002904d00e1d
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-03 16:28:25 +0200

    learn about nesting multiple components and display them

---
+++ vite-project/src/reactLearn/MyApp.jsx
@@ -0,0 +1,11 @@
+function MyButton() {
+    return <><button type="button">Awake me 😴😴</button></>
+}
+
+export default function MyApp() {
+    return (
+        <>
+            <h1>Welcome to my app my pipo</h1>
+            <MyButton />
+        </>);
+}
