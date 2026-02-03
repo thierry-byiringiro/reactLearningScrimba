464bb14c343800858460ea92b175a9e108b5c504
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-03 17:28:45 +0200

    favorites array and access the items by using lists

--- vite-project/src/reactLearn/Profile.jsx
+++ vite-project/src/reactLearn/Profile.jsx
@@ -5,10 +5,18 @@ const Profile = () => {
         imageSize: 90
     }
 
+    const favorites = [
+        { title: 'Football',type: "Man U" },
+        { title: 'Movie',type : 'Stranger Things' },
+        { title: 'Food',type : "French fries" }
+    ];
+    
+    let isMale = false;
     return (
         <>
-            <h1>Holla,{user.name}</h1>
-            <img src={user.imageUrl} alt={`Photo of ${user.name}`} style={{ width: user.imageSize, height: user.imageSize }} />
+            <h1>Holla ,{isMale ? ("Mr.") : ("Mrs")} {user.name}</h1>
+            <img src={user.imageUrl} alt={`Photo of ${user.name}`} style={{ width: user.imageSize, height: user.imageSize, borderRadius: 30 }} />
+            <h3>Below is the list of my favorites</h3>
         </>
     );
 
