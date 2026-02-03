b298d7a1d125c04466864a02523f8f7a94c603e5
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-03 18:17:55 +0200

    learn about using states especially useState

--- vite-project/src/reactLearn/MyApp.jsx
+++ vite-project/src/reactLearn/MyApp.jsx
@@ -1,11 +1,21 @@
-function MyButton() {
-    return <><button type="button">Awake me 😴😴</button></>
-}
+import { useState } from "react";
 
-export default function MyApp() {
+function MyApp() {
+    let [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+    function MyButton() {
+        return <><button type="button" onClick={handleClick}>Awake me 😴😴</button></>
+    }
     return (
         <>
             <h1>Welcome to my app my pipo</h1>
             <MyButton />
+            <h2>
+                {count}
+            </h2>
         </>);
 }
+export default MyApp;
