5779de945277b037e236af1693737389ee3d00e6
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-08 18:27:45 +0200

    learn about sharing data between components

--- vite-project/src/reactLearn/MyApp.jsx
+++ vite-project/src/reactLearn/MyApp.jsx
@@ -1,28 +1,27 @@
 import { useState } from "react";
 
+function MyButton({count,onclick}) {
+    return (
+        <>
+            <div className="myBut">
+                <button type="button" onClick={onclick}>Clicked {count} time(s)</button>
+            </div>
+        </>
+    )
+}
+
 function MyApp() {
     const [count, setCount] = useState(0);
 
     const increment = () => {
         setCount(count + 1);
     }
-    function MyButton() {
-        return (
-            <>
-                <div className="myBut">
-                    <button type="button" onClick={increment}>Button 1</button>
-                    <button type="button" onClick={increment}>Button 2</button>
-                </div>
-            </>
-        )
-    }
+
     return (
         <>
             <h1>Welcome to my app my pipo</h1>
-            <MyButton />
-            <h2>
-                {count}
-            </h2>
+            <MyButton count={count} onclick={increment}/><br />
+            <MyButton count={count} onclick={increment} />
         </>);
 }
 export default MyApp;
