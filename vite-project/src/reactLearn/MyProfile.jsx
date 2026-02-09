13f9e0a4a7e7a381af1ae234e9af9ce2be2dce91
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-09 11:47:00 +0200

    learn props and passing from parent to the child element

--- vite-project/src/reactLearn/MyProfile.jsx
+++ vite-project/src/reactLearn/MyProfile.jsx
@@ -1,16 +1,11 @@
 import { getImageUrl } from "./utils";
-function Avatar({person,size}){
-    return (
-    <>
-        < img className="avatar" src={getImageUrl(person)} alt={person.name} width={size} height={size}/>
-    </>
-    );
-}
 
 export default function MyProfile(){
     return (
         <>
-            <Avatar person={{name : "aime",imageId:'1bX5QH6'}} size={100}/>
+            <Avatar person={{name :"Katsuko Saruhashi",imageId: "YfeOqp2"}} size={100}/>
+            <Avatar person={{name : "Aklilu Lemma",imageId: "OKS67lh"}} size={80}/>
+            <Avatar person={{name : "Lin Lanying",imageId:'1bX5QH6'}} size={50}/>
         </>
     );
 }
