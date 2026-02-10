03adb5ffb786f44a26a74700135a96ba5061a3b4
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-10 20:28:14 +0200

    additional pages

--- vite-project/src/reactLearn/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import { getImageUrl } from "./utils";
-function Avatar({ person, size }) {
-    return (
-        <>
-            < img className="avatar" src={getImageUrl(person)} alt={person.name} width={size} height={size} />
-        </>
-    );
-}
-export default Avatar;
--- vite-project/src/reactLearn/PropsTry.jsx
+++
@@ -1,7 +0,0 @@
-export default function PropsTry({name}) {
-    return (
-    <>
-    <h1>Hello {name}</h1>
-    </>
-    );
-}
