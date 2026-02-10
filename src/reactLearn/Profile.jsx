60cdd0682fc062169710dfe736c39d1c1dc9662b
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-10 20:25:55 +0200

    tryin to display different props

---
+++ src/reactLearn/Profile.jsx
@@ -0,0 +1,31 @@
+const Profile = () => {
+    const user = {
+        name: "Aime Thierry Byiringiro",
+        imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+        imageSize: 90
+    }
+
+    const favorites = [
+        { title: 'Football',type: "Man U" },
+        { title: 'Movie',type : 'Stranger Things' },
+        { title: 'Food',type : "French fries" }
+    ];
+
+    let getFavorites = favorites.map((element,index) => (
+        <li key ={index} style={{color : element.title.startsWith("F") ? "blue" : "red"}}>{element.title} {`-->`} {element.type}</li>
+    ))
+    let isMale = true;
+    return (
+        <>
+            <h1>Holla ,{isMale ? ("Mr.") : ("Mrs")} {user.name}</h1>
+            <img src={user.imageUrl} alt={`Photo of ${user.name}`} style={{ width: user.imageSize, height: user.imageSize, borderRadius: 30 }} />
+            <h3>Below is the list of my favorites</h3>
+            <ul>
+                {getFavorites}
+            </ul>
+        </>
+    );
+
+}
+
+export default Profile;
